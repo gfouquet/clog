2a5933405203784b446534284b8c7f4b73bf2c50
Grégory Fouquet <gfouquet@armida.fr>
2015-12-01 20:43:39 +0100

    Created LogFile React component to which lines can be appended

--- javascripts/entry.jsx
+++ javascripts/entry.jsx
@@ -1,15 +1,31 @@
 'use strict';
 // from https://medium.com/@Agro/developing-desktop-applications-with-electron-and-react-40d117d97564#.lqp9n9qfy
-const React = require("react");
-const ReactDOM = require("react-dom");
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 const LogLine = React.createClass({
 	render() {
-		return <div>{ this.props.text }</div>;
+		return <li>{ this.props.text }</li>;
 	}
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-	ReactDOM.render(<LogLine text='Hello clog' />, document.querySelector('.main'));
+const LogFile = React.createClass({
+	getInitialState() {
+		return { lines: [] };
+	},
+	render() {
+		const logLines = this.state.lines.map((line, idx) => <LogLine text={ line } key={ idx } />);
+		return <ul>{ logLines }</ul>;
+	},
+	append(line) {
+		this.state.lines.push(line);
+		this.setState(this.state);
+	}
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+	const logFile = ReactDOM.render(<LogFile />, document.querySelector('.main'));
+	logFile.append('Hello');
+	logFile.append('Clog');
 });
 
